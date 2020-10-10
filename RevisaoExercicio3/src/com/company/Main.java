478b3b338148c67d3db505f9dca080a4d36a3e6b
Kaique Toledo <38433930+imperialjah@users.noreply.github.com>
2020-10-10 10:09:18 -0300

    Exercício 3 da lista de Revisão

---
+++ RevisaoExercicio3/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Porta porta = new Porta();
+
+        porta.setAberta(false);
+        porta.setCor("Branca");
+        porta.setDimensaoX(90);
+        porta.setDimensaoY(180);
+        porta.setDimensaoZ(4);
+
+        porta.abre();
+
+        porta.fecha();
+
+        porta.pinta("Verde");
+        porta.pinta("Azul");
+        porta.pinta("Preta");
+
+        porta.setDimensaoZ(5);
+        porta.setDimensaoX(95);
+        porta.setDimensaoY(190);
+
+        boolean estaAberta = porta.isAberta();
+        if(estaAberta) {
+            System.out.println("A porta " + porta.getCor() + " esta aberta");
+        } else {
+            System.out.println("A porta " + porta.getCor() + " esta fechada");
+        }
+    }
+}
